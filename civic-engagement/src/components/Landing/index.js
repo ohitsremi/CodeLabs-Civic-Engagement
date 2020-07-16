3e66ef07ef3a7832724049e21b09268472517b76
Remi Srijunyanont <psrijuny@ucsc.edu>
2020-07-16 01:08:25 -0400

    Register (Step 1)

--- civic-engagement/src/components/Landing/index.js
+++ civic-engagement/src/components/Landing/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import "bootswatch/dist/minty/bootstrap.min.css"; 
  
 const App = () => (
-  <div>
-    <h1>Landing</h1>
+  <div class="text-center">
+    <p>
+      Hi! Welcome to <a href="#">EngageForChange</a>.
+    </p>
+    <p>Tell us a little bit about yourself.</p>
+
+    <h3>I am a...</h3>
+    <button type="button" class="btn btn-primary btn-lg btn-block">VOLUNTEER</button>
+    <button type="button" class="btn btn-outline-primary btn-lg btn-block">VOLUNTEER ORGANIZATION</button>
   </div>
+  
+  
 );
  
 export default App;
