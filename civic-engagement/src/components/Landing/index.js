b02001c5e78f7b8f7d18ad285a5f37c4c196fefb
Remi Srijunyanont <psrijuny@ucsc.edu>
2020-07-16 17:01:44 -0400

    Volunteer register page

--- civic-engagement/src/components/Landing/index.js
+++ civic-engagement/src/components/Landing/index.js
@@ -1,16 +1,20 @@
 import React from 'react';
-import "bootswatch/dist/minty/bootstrap.min.css"; 
+import { Link } from 'react-router-dom';
+import "bootswatch/dist/minty/bootstrap.min.css";
+import * as ROUTES from '../../constants/routes'; 
  
 const App = () => (
-  <div class="text-center">
+  <div class="container-fluid">
     <p>
-      Hi! Welcome to <a href="#">EngageForChange</a>.
+      Hi! Welcome to <span class="text-primary">EngageForChange</span>.
     </p>
     <p>Tell us a little bit about yourself.</p>
 
     <h3>I am a...</h3>
-    <button type="button" class="btn btn-primary btn-lg btn-block">VOLUNTEER</button>
+    <Link to={ROUTES.VOLUNTEER}><button type="button" class="btn btn-primary btn-lg btn-block">VOLUNTEER</button></Link>
     <button type="button" class="btn btn-outline-primary btn-lg btn-block">VOLUNTEER ORGANIZATION</button>
+    
+
   </div>
   
   
