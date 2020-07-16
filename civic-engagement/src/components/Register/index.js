89251347cb2404ae6efe5cfdcca4ac0520bcc915
Remi Srijunyanont <psrijuny@ucsc.edu>
2020-07-16 15:00:05 -0400

    Update index.js

---
+++ civic-engagement/src/components/Register/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "bootswatch/dist/minty/bootstrap.min.css"; 
+ 
+const App = () => (
+  <div class="text-center">
+    <p>
+      I am a... Volunteer
+    </p>
+  </div>
+  
+  
+);
+ 
+export default App;
