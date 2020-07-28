67b28a6222241f8feb11e95b65f16416612112bd
Remi Srijunyanont <psrijuny@ucsc.edu>
2020-07-28 11:35:45 -0400

    Code Clean up
    Removed files and routes that were not used.

--- civic-engagement/src/components/App/index.js
+++ civic-engagement/src/components/App/index.js
@@ -3,17 +3,12 @@ import {BrowserRouter as Router, Route,} from 'react-router-dom';
 
 import Navigation from '../Navigation';
 import LandingPage from '../Landing';
-import SignUpPage from '../SignUp';
-import SignInPage from '../SignIn';
-import PasswordForgetPage from '../PasswordForget';
 import HomePage from '../Home';
-import AccountPage from '../Account';
 import AdminPage from '../Admin';
 import VolunteerPage from '../Volunteer';
 import DirectoryPage from '../Directory';
 
 import * as ROUTES from '../../constants/routes';
-import { withAuthentication } from '../Session';
 
 const App = () => (
   <Router>
@@ -23,14 +18,7 @@ const App = () => (
       <hr />
 
       <Route exact path={ROUTES.LANDING} component={LandingPage} />
-      <Route exact path={ROUTES.SIGN_UP} component={SignUpPage} />
-      <Route exact path={ROUTES.SIGN_IN} component={SignInPage} />
-      <Route
-        exact path={ROUTES.PASSWORD_FORGET}
-        component={PasswordForgetPage}
-      />
       <Route exact path={ROUTES.HOME} component={HomePage} />
-      <Route exact path={ROUTES.ACCOUNT} component={AccountPage} />
       <Route exact path={ROUTES.ADMIN} component={AdminPage} />
       <Route exact path={ROUTES.VOLUNTEER} component={VolunteerPage}/>
       <Route exact path ={ROUTES.DIRECTORY} component={DirectoryPage}/>
@@ -38,4 +26,4 @@ const App = () => (
   </Router>
 );
  
-export default withAuthentication(App);
+export default App;
